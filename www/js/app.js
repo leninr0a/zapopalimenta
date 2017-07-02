5113c8117c8a044317cbd6755136e6b4066ccf8d
leninr0a <lenin_513@hotmail.com>
2017-07-02 03:51:15 -0500

    added donate view and form functionality

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic','starter.controllers', 'starter.services'])
+angular.module('starter', ['ionic','starter.controllers', 'starter.services', 'ionic-datepicker'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -23,7 +23,27 @@ angular.module('starter', ['ionic','starter.controllers', 'starter.services'])
   });
 })
 
-.config(function($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider, ionicDatePickerProvider) {
+
+        var datePickerObj = {
+          inputDate: new Date(),
+          titleLabel: 'Selecciona una Fecha',
+          setLabel: 'Aceptar',
+          todayLabel: 'Hoy',
+          closeLabel: 'Cerrar',
+          mondayFirst: false,
+          weeksList: ["L", "M", "M", "J", "V", "S", "D"],
+          monthsList: ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Sept", "Oct", "Nov", "Dec"],
+          templateType: 'popup',
+          from: new Date(2012, 8, 1),
+          to: new Date(2018, 8, 1),
+          showTodayButton: true,
+          dateFormat: 'dd MMMM yyyy',
+          closeOnSelect: false,
+          disableWeekdays: []
+        };
+
+        ionicDatePickerProvider.configDatePicker(datePickerObj);
 
         $stateProvider
             .state('home', {
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,15 +1,96 @@
 angular.module('starter.controllers', [])
 
 
-/*Controller Home*/
-.controller('homeCtrl', function($http,$scope){
-	console.log("hola mundo");
+/*Home Controller*/
+.controller('homeCtrl', function($http,$scope,$window){
+	console.log("hola mundo desde ");
+
+	$scope.goDonate = function(){
+		$window.location.assign('#/donate');
+	};
+
+	$scope.goRecolect = function(){
+		$window.location.assign('#/recolect');
+	};
+
 
 })
 
 
-/*Donate Ctrl*/
-.controller('donateCtrl', function($http,$scope){
-	console.log("hola mundo");
+/*Donate Controller*/
+.controller('donateCtrl', function($http,$scope, ionicDatePicker){
+	
+	$scope.expirationDate;
+	$scope.food={};
+	$scope.foods = [];
+
+	$scope.addFood = function(){
+		$scope.foods.push($scope.food);	
+		console.log($scope.foods);
+		$scope.food={};
+	}
+
+	$scope.removeFood = function(index){
+		$scope.foods.splice(index,1);
+	}
+
+	 var ipObj1 = {
+      callback: function (val) { 
+      	var date = new Date(val);
+      	$scope.food.expirationDate = dateToString(date);
+      },
+      templateType: 'popup'       //Optional
+    };
+
+    var dateToString = function(date){
+      	var month = date.getMonth();
+    	switch(month){
+    		case 0:
+    			month = "Enero";
+    			break;
+    		case 1:
+    			month = "Febrero";
+    			break;
+    		case 2:
+    			month = "Marzo";
+    			break;
+    		case 3:
+    			month = "Abril";
+    			break;
+    		case 4:
+    			month = "Mayo";
+    			break;
+    		case 5:
+    			month = "Junio";
+    			break;
+    		case 6:
+    			month = "Julio";
+    			break;
+    		case 7:
+    			month = "Agosto";
+    			break;
+    		case 8:
+    			month = "Septiembre";
+    			break;
+    		case 9:
+    			month = "Octubre";
+    			break;
+    		case 10:
+    			month = "Noviembre";
+    			break;
+    		case 11:
+    			month = "Diciembre";
+    			break;
+    	}	
+
+		return date.getDate() + " de " + month + " del " + date.getFullYear();
+    }
+
+
+
+	$scope.openDatePicker = function(){
+		console.log("openDatePicker");
+      ionicDatePicker.openDatePicker(ipObj1);
+    };
 
 })
