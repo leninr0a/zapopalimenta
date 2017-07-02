f4b6d16435d54433fbff76c3875abc7c1e772ec0
Lenin Roa <lenin_513@hotmail.com>
2017-07-01 21:38:57 -0500

    initialize zapopalomenta

---
+++ www/js/controllers.js
@@ -0,0 +1,15 @@
+angular.module('starter.controllers', [])
+
+
+/*Controller Home*/
+.controller('homeCtrl', function($http,$scope){
+	console.log("hola mundo");
+
+})
+
+
+/*Donate Ctrl*/
+.controller('donateCtrl', function($http,$scope){
+	console.log("hola mundo");
+
+})
