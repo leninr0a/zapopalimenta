73ff594e1cda686ae5dcce74cf91e25628e06928
leninr0a <lenin_513@hotmail.com>
2017-07-03 01:14:30 -0500

    added geolocation and address view

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic','starter.controllers', 'starter.services', 'ionic-datepicker'])
+angular.module('starter', ['ionic','starter.controllers', 'starter.services', 'ionic-datepicker','ngCordova'])
 
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -56,5 +56,15 @@ angular.module('starter', ['ionic','starter.controllers', 'starter.services', 'i
                 templateUrl: '/templates/donar.html',
                 controller: 'donateCtrl'
                 })
+            .state('donate-details', {
+                url: '/donate-details',
+                templateUrl: '/templates/donate-details.html',
+                controller: 'donateDetailsCtrl'
+                })
+             .state('map', {
+                url: '/map',
+                templateUrl: 'templates/map.html',
+                controller: 'mapCtrl'
+              });
          $urlRouterProvider.otherwise('/home');
 })
--- www/js/controllers.js
+++ www/js/controllers.js
@@ -18,11 +18,34 @@ angular.module('starter.controllers', [])
 
 
 /*Donate Controller*/
-.controller('donateCtrl', function($http,$scope, ionicDatePicker){
+.controller('donateCtrl', function($http,$scope, ionicDatePicker,  $cordovaCamera, $ionicPopup, $window){
 	
 	$scope.expirationDate;
 	$scope.food={};
 	$scope.foods = [];
+	 
+
+ 	$scope.takePhoto = function () {
+                  var options = {
+                    quality: 75,
+                    destinationType: Camera.DestinationType.DATA_URL,
+                    sourceType: Camera.PictureSourceType.CAMERA,
+                    allowEdit: true,
+                    encodingType: Camera.EncodingType.JPEG,
+                    targetWidth: 300,
+                    targetHeight: 300,
+                    popoverOptions: CameraPopoverOptions,
+                    saveToPhotoAlbum: false
+                };
+   
+                    $cordovaCamera.getPicture(options).then(function (imageData) {
+                        $scope.imgURI = "data:image/jpeg;base64," + imageData;
+                        console.log($scope.imgURI);
+                    }, function (err) {
+                        // An error occured. Show a message to the user
+                    });
+                }
+  
 
 	$scope.addFood = function(){
 		$scope.foods.push($scope.food);	
@@ -31,7 +54,24 @@ angular.module('starter.controllers', [])
 	}
 
 	$scope.removeFood = function(index){
-		$scope.foods.splice(index,1);
+     $scope.showConfirm = function() {
+	   var confirmPopup = $ionicPopup.confirm({
+	     title: 'Borrar alimento',
+	     template: '¿Est&aacute;s seguro de querer borrar este alimento?',
+	      buttons: [
+                {
+                  text: '<b>S&iacute;</b>',
+                  type: 'button-positive',
+                  onTap: function(e) {
+				       $scope.foods.splice(index,1);
+                  }
+                },
+                { text: 'Cancelar', onTap: function(e) { return true; } },
+              ]
+	   });
+	 };
+	 $scope.showConfirm();
+
 	}
 
 	 var ipObj1 = {
@@ -92,5 +132,148 @@ angular.module('starter.controllers', [])
 		console.log("openDatePicker");
       ionicDatePicker.openDatePicker(ipObj1);
     };
+
+    $scope.goDonateDetails = function(){
+		$window.location.assign('#/donate-details');
+	};
+
+
+
+
+})
+
+/*Donate Details Controller*/
+.controller('donateDetailsCtrl', function($http,$scope,$window,$ionicModal, $cordovaGeolocation){
+	$scope.adresses = [
+		{
+			street1: 'Calle Axayacatl 118',
+			street2: 'Cd del Sol',
+			city: '45050 Zapopan, Jal',
+			phone: '335570092854'
+		},
+		{
+			street1: 'Calle Cuitláhuac 4096',
+			street2: 'Cd del Sol',
+			city: '45050 Zapopan, Jal',
+			phone: '335570092854',
+		},
+		{
+			street1: 'Av. San Luis Gonzaga 5026',
+			street2: 'Jardines de Guadalupe',
+			city: '45050 Zapopan, Jal',
+			phone: '335570092854',
+		}
+	];
+
+		 $ionicModal.fromTemplateUrl('templates/address-modal.html', {
+    scope: $scope,
+    animation: 'slide-in-up'
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  $scope.openModal = function() {
+    $scope.modal.show();
+    $scope.generateMap();
+  };
+  $scope.closeModal = function() {
+    $scope.modal.hide();
+  };
+  // Cleanup the modal when we're done with it!
+  $scope.$on('$destroy', function() {
+    $scope.modal.remove();
+  });
+  // Execute action on hide modal
+  $scope.$on('modal.hidden', function() {
+    // Execute action
+  });
+  // Execute action on remove modal
+  $scope.$on('modal.removed', function() {
+    // Execute action
+  });
+
+
+	$scope.showModal = function(){
+
+
+	 
+  	$scope.openModal();
+
+  	};
+
+	$scope.goDonate = function(){
+		$window.location.assign('#/donate');
+	};
+
+	$scope.goRecolect = function(){
+		$window.location.assign('#/recolect');
+	};
+
+	var options = {timeout: 10000, enableHighAccuracy: true};
+ 
+  $scope.generateMap = function(){
+  	var marker;
+  	var geocoder = new google.maps.Geocoder;
+  	$cordovaGeolocation.getCurrentPosition(options).then(function(position){
+ 
+    var latLng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+ 
+    var mapOptions = {
+      center: latLng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+ 
+    $scope.map = new google.maps.Map(document.getElementById("map"), mapOptions);
+ 	
+ 	google.maps.event.addListener($scope.map, 'click', function(event) {
+    var lat = event.latLng.lat();
+    var lng = event.latLng.lng();
+    // populate yor box/field with lat, lng
+    console.log("Lat=" + lat + "; Lng=" + lng);
+    placeMarker(event.latLng);
+    getAdress(event.latLng);
+	});
+
+
+
+	function placeMarker(location) {
+	      if ( marker ) {
+		    marker.setPosition(location);
+		  } else {
+		    marker = new google.maps.Marker({
+		      position: location,
+		      map: $scope.map
+		    });
+			}
+
+	}
+
+
+	function getAdress(location){
+		 geocoder.geocode({'location': location}, function(results, status) {
+          if (status === 'OK') {
+            if (results[0]) {
+              console.log(results[0].formatted_address);
+			}
+		   }
+		});
+	}
+
+  }, function(error){
+    console.log("Could not get location");
+  });
+  
+
+
+  }
+
+
+})
+
+
+
+/*Map Controller*/
+.controller('mapCtrl', function($http,$scope, ionicDatePicker,  $cordovaCamera, $ionicPopup, $window, $cordovaGeolocation){
 
 })
